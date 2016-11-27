d205d6ab64ba4ad4b30693bcd34d4ecd6cf80abc
jprchlik <japrchlik@gmail.com>
2016-11-27 13:07:57 -0500

    Added parallel processing

--- script_images.py
+++ script_images.py
@@ -1,6 +1,16 @@
 import plot_chandra_image as pci
+from multiprocessing import Pool
 
-chan = pci.format_image(9919)
+def make_image(epoch):
+    chan = pci.format_image(epoch)
+    
+    chan.make_plot(starreg=True,backreg=True)
 
-chan.make_plot(starreg=True)
+    
+
+epochs = [9919,9920,10809,10810,10811,10812]
+
+pool = Pool(processes=4)
+out = pool.map(make_image,epochs)
+out.close()
 
