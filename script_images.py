f7348785665bc9879f5305372539813ff31091db
jprchlik <japrchlik@gmail.com>
2016-11-30 22:07:11 -0500

    Readded parallel processing

--- script_images.py
+++ script_images.py
@@ -4,13 +4,14 @@ from multiprocessing import Pool
 def make_image(epoch):
     chan = pci.format_image(epoch)
     
-    chan.make_plot(starreg=True,backreg=True)
+    chan.make_plot(starreg=True,backreg=True,writreg=True)
 
     
 
 epochs = [9919,9920,10809,10810,10811,10812]
+#make_image(epochs[0])
 
 pool = Pool(processes=4)
 out = pool.map(make_image,epochs)
-out.close()
+pool.close()
 
