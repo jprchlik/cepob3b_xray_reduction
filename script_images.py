7607c14dee32f19c1871f0b4687c7cf6e186801c
jprchlik <japrchlik@gmail.com>
2016-12-04 11:49:42 -0500

    Reintroducted multiprocessing

--- script_images.py
+++ script_images.py
@@ -5,14 +5,14 @@ def make_image(epoch):
     chan = pci.format_image(epoch)
     
     chan.make_plot(starreg=True,backreg=True,writreg=True)
-#    chan.run_extraction()
+    chan.run_extraction()
 
     
 
 epochs = [9919,9920,10809,10810,10811,10812]
-make_image(epochs[0])
+#make_image(epochs[0])
 
-#pool = Pool(processes=4)
-#out = pool.map(make_image,epochs)
-#pool.close()
+pool = Pool(processes=4)
+out = pool.map(make_image,epochs)
+pool.close()
 
