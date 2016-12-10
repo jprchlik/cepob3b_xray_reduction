a0c857b6876632a4eb22b8d2c2935c75d39890dc
jprchlik <japrchlik@gmail.com>
2016-12-10 11:04:37 -0500

    Removed outdated comments and lines

--- pfit_all.py
+++ pfit_all.py
@@ -60,7 +60,7 @@ def main(argv):
     outf.write('{8:^10}{0:^10}{1:^10}{2:^10}{3:^10}{4:^10}{5:^10}{6:^10}{7:^10}{9:^10}{10:^10}{11:^10}{12:^10}{13:^10}{14:^10}{15:^10}{16:^10}{17:^10}{18:^10}\n'.format('uflux','uflux_err','aflux','aflux_err','cnts',' cnts_err',' ncnts',' ncnts_err','src','ukT','unH','ukterr','unHerr','uchi','urstat','akT','anH','achi','arstat'))
     for j in np.arange(1,len(files)+1):
 #    for j in np.arange(1,10):
-#       try:
+       try:
             exam = 'sers_{0:4d}'.format(j).replace(' ','0')
             
             
@@ -87,9 +87,6 @@ def main(argv):
                 subtract()
 #group points in bins of at least 10
                 group_counts(8)
-#            save_grouping(sfile+"_group_10.pi",ascii=False,clobber=True)
-#applying group to backgroud
-#            set_grouping(1,get_grouping(1),bkg_id=1)
             
             
             data_sum = calc_data_sum(mine,maxe)  
@@ -127,31 +124,8 @@ def main(argv):
 #            set_full_model(umod)
             thaw(abs1)
             set_source(umod)
-            #guess initial model and fit
-#            guess(abs1)
-#            guess(b1)
 #set the maximum and minimum range to fit
             notice(mine,maxe)
-#Set up back ground and background model
-#            set_bkg(bkg)
-##setup model components from Brad Spitzerbar (constant,power law, and Gaussian)
-#            set_bkg_model(abs1*xsconstant.c*xspowerlaw.p1*xsgaussian.g1)
-##set up backgroud model parameters
-#            c.factor = 0.0
-#            p1.PhoIndex=2
-#            p1.norm=0.
-#            g1.LineE=0.
-#            g1.Sigma=.0
-#            g1.norm=0.0
-#            
-#           
-##freeze in backgroud parameters
-#            freeze(c.factor)
-#            freeze(p1.PhoIndex)
-#            freeze(p1.norm)
-#            freeze(g1.LineE)
-#            freeze(g1.Sigma)
-#            freeze(g1.norm)
 
 #fit the model
             fit()
@@ -163,11 +137,6 @@ def main(argv):
             ####calc_stat_info()
             uchi = get_fit_results().statval
             urst = get_fit_results().rstat
-#            ugoodness=get_stat_info()
-#            uchi = ugoodness[0].statval
-#            urst = ugoodness[0].rstat
-            #Calculate Error for model using 100 samples
-#            unabs = sample_flux(umod,mine,maxe,num=100)
 #Switch from total model to just xsraymond model because of Doug Burke (2016/12/07)
             unabs = sample_flux(b1,mine,maxe,num=1000)
 	    #      save best fit model as fits file
@@ -193,47 +162,10 @@ def main(argv):
             
 
             
-###            #Winston 2010 used xsraymond
-###            amod = xsvapec.b1
-###            set_stat(stat) 
-###            set_source(amod)
-###            b1.cache=0
-###            b1.kT = 1.5 #kT
-###            b1.kT.max = 50.
-###            b1.kT.min = 0.01
-###            #guess initial model and fit
-###            guess(b1)
-#setup model components from Brad Spitzerbar (constant,power law, and Gaussian)
-#            set_bkg_model(xsconstant.c*xspowerlaw.p1*xsgaussian*g1)
-##freeze in previous backgroud parameters
-#            freeze(c.factor)
-#            freeze(p1.PhoIndex)
-#            freeze(p1.norm)
-#            freeze(g1.LineE)
-#            freeze(g1.Sigma)
-#            freeze(g1.norm)
-
-####            fit()
-            #plot the abs fit
-#            plot_fit(overplot=True)
-            #change the abs fit color to blue
-#            set_curve("crv4",["*.color","blue"])
-#            #best fit values absorbed values
-#Commented out seperate unattenuated fit because of Doug Burke (2016/12/07)
-###            akt,anorm = get_fit_results().parvals
-###            anh = 0.0
-###            ####calc_stat_info()
-###            achi = get_fit_results().statval
-###            arst = get_fit_results().rstat
+#No longer fit abs parameters
             akt, anorm = -99.9,-99.9
             anh = 0.0
             achi,arst = -99.9,-99.9
-###            
-###            
-###            #Calculate Error for model using 100 samples
-###            anabs = sample_flux(amod,mine,maxe,num=100)
-####      save best fit model as fits file
-###            save_model(sfile+"_{0}_amod.pi".format(stat),ascii=False,clobber=True)
 ####get the total fluxes and errors in a 3D vector 
             anabs = unabs[0]
             unabs = unabs[1]
@@ -256,9 +188,9 @@ def main(argv):
         
             outf.write('{8:^10d}{0:^10.3e}{1:^10.3e}{2:^10.3e}{3:^10.3e}{4:^10.1f}{5:^10.1f}{6:^10.1f}{7:^10.1f}{9:^10.4f}{10:^10.4f}{17:^10.4f}{18:^10.4f}{11:^10.4f}{12:^10.4f}{13:^10.4f}{14:^10.4f}{15:^10.4f}{16:^10.4f}\n'.format(unabs,unabs_err,anabs,anabs_err,data_sum,data_sum_err,data_sum-bkg_sum,tot_c_err,j,ukt,unh,uchi,urst,akt,anh,achi,arst,ukTerr,unHerr))
         
-#       except:
-#########Write out -9999 if cannot find solution
-#            outf.write('{8:^10d}{0:^10d}{1:^10d}{2:^10d}{3:^10d}{4:^10d}{5:^10d}{6:^10d}{7:^10d}{9:^10d}{10:^10d}{11:^10d}{12:^10d}{13:^10d}{14:^10d}{15:^10d}{16:10d}{17:10d}{18:10d}\n'.format(-999,-9999,-9999,-9999,-9999,-9999,-9999,-9999,j,-9999,-9999,-9999,-9999,-9999,-9999,-9999,-9999,-9999,-9999,-9999))
+       except:
+########Write out -9999 if cannot find solution
+            outf.write('{8:^10d}{0:^10d}{1:^10d}{2:^10d}{3:^10d}{4:^10d}{5:^10d}{6:^10d}{7:^10d}{9:^10d}{10:^10d}{11:^10d}{12:^10d}{13:^10d}{14:^10d}{15:^10d}{16:10d}{17:10d}{18:10d}\n'.format(-999,-9999,-9999,-9999,-9999,-9999,-9999,-9999,j,-9999,-9999,-9999,-9999,-9999,-9999,-9999,-9999,-9999,-9999,-9999))
     outf.close()
 
 if __name__ == "__main__":
