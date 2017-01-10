88c694a67fafc7d427769637a59556d0ee538e2f
jprchlik <japrchlik@gmail.com>
2017-01-09 23:59:38 -0500

    Added RA and Dec to output

--- pfit_all.py
+++ pfit_all.py
@@ -5,6 +5,8 @@ from sherpa.astro.utils import _charge_e as q # ~1.6e-9 ergs/1 keV photon (nist.
 import glob
 import sys
 import getopt
+from crates_contrib.utils import SimpleCoordTransform
+import sherpa_contrib.utils as utils
 
 
 #load_pha("3c273.pi")
@@ -51,21 +53,27 @@ def main(argv):
 
     set_analysis("energy")
     files = glob.glob(i+diri+'*bkg.pi')
-#create an average background
+ 
+#Get image file for coordinates
+    tr = SimpleCoordTransform(i+'/obs'+i+'_img.fits')
+
+
 
 
 
 
 #    files = files[:1]
     outf = open(i+'/prelim_out_{1}_{0:5d}.dat'.format(int(i),stat).replace(' ','0'),'w')
-    outf.write('{8:^10}{0:^10}{1:^10}{2:^10}{3:^10}{4:^10}{5:^10}{6:^10}{7:^10}{9:^10}{10:^10}{11:^10}{12:^10}{13:^10}{14:^10}{15:^10}{16:^10}{17:^10}{18:^10}\n'.format('uflux','uflux_err','aflux','aflux_err','cnts',' cnts_err',' ncnts',' ncnts_err','src','ukT','unH','ukterr','unHerr','uchi','urstat','akT','anH','achi','arstat'))
-    for j in np.arange(1,len(files)+1):
-#    for j in np.arange(1,10):
+    outf.write('{8:^10}{0:^10}{1:^10}{2:^10}{3:^10}{4:^10}{5:^10}{6:^10}{7:^10}{9:^10}{10:^10}{11:^10}{12:^10}{13:^10}{14:^10}{15:^10}{16:^10}{17:^10}{18:^10}{19:^15}{20:^15}\n'.format('uflux','uflux_err','aflux','aflux_err','cnts',' cnts_err',' ncnts',' ncnts_err','src','ukT','unH','ukterr','unHerr','uchi','urstat','akT','anH','achi','arstat','RA','Dec'))
+    for j in np.arange(len(files)):
+#    for j in np.arange(2):
        try:
+#write same integer to file srcnumber  (J. Prchlik 2017/01/05)
+            j = int(files[j].replace(i+diri,'').split('_')[1])
             exam = 'sers_{0:4d}'.format(j).replace(' ','0')
-            
-            
-    
+
+
+
             sfile = i+diri+exam
             #un#subtracted source spectrum
             load_pha(sfile+".pi")
@@ -83,11 +91,17 @@ def main(argv):
             bkg_scal = get_bkg_scale()
             set_backscal(bkg_scal)
             #Fix background scaling of rmf and arf when fitting (2017/01/02 J. Prchlik)
-            load_bkg_arf(arf.name)
-            load_bkg_rmf(rmf.name)
-            barf = get_bkg_arf()
-            brmf = get_bkg_rmf()
-            print brmf.name
+#            load_bkg_arf(arf.name)
+#            load_bkg_rmf(rmf.name)
+#            barf = get_bkg_arf()
+#            brmf = get_bkg_rmf()
+           
+#Source ra and dec
+            tcr = utils.pycrates.read_file(sfile+"_srcreg.fits[cols x,y]")
+            sx1 = utils.pycrates.copy_colvals(tcr,"x")
+            sy1 = utils.pycrates.copy_colvals(tcr,"y")
+            (ra, dec) = tr.convert("sky","world",sx1[0],sy1[0])
+            ra,dec = float(ra),float(dec)
             
 #            notice_id(1,mine,maxe)
             if chid:
@@ -198,11 +212,11 @@ def main(argv):
             tot_c_err = 0.0
 #write output to file
         
-            outf.write('{8:^10d}{0:^10.3e}{1:^10.3e}{2:^10.3e}{3:^10.3e}{4:^10.1f}{5:^10.1f}{6:^10.1f}{7:^10.1f}{9:^10.4f}{10:^10.4f}{17:^10.4f}{18:^10.4f}{11:^10.4f}{12:^10.4f}{13:^10.4f}{14:^10.4f}{15:^10.4f}{16:^10.4f}\n'.format(unabs,unabs_err,anabs,anabs_err,data_sum,data_sum_err,data_sum-bkg_sum,tot_c_err,j,ukt,unh,uchi,urst,akt,anh,achi,arst,ukTerr,unHerr))
+            outf.write('{8:^10d}{0:^10.3e}{1:^10.3e}{2:^10.3e}{3:^10.3e}{4:^10.1f}{5:^10.1f}{6:^10.1f}{7:^10.1f}{9:^10.4f}{10:^10.4f}{17:^10.4f}{18:^10.4f}{11:^10.4f}{12:^10.4f}{13:^10.4f}{14:^10.4f}{15:^10.4f}{16:^10.4f}{19:^15.8f}{20:^15.8f}\n'.format(unabs,unabs_err,anabs,anabs_err,data_sum,data_sum_err,data_sum-bkg_sum,tot_c_err,j,ukt,unh,uchi,urst,akt,anh,achi,arst,ukTerr,unHerr,ra,dec))
         
        except:
 ########Write out -9999 if cannot find solution
-            outf.write('{8:^10d}{0:^10d}{1:^10d}{2:^10d}{3:^10d}{4:^10d}{5:^10d}{6:^10d}{7:^10d}{9:^10d}{10:^10d}{11:^10d}{12:^10d}{13:^10d}{14:^10d}{15:^10d}{16:10d}{17:10d}{18:10d}\n'.format(-999,-9999,-9999,-9999,-9999,-9999,-9999,-9999,j,-9999,-9999,-9999,-9999,-9999,-9999,-9999,-9999,-9999,-9999,-9999))
+            outf.write('{8:^10d}{0:^10d}{1:^10d}{2:^10d}{3:^10d}{4:^10d}{5:^10d}{6:^10d}{7:^10d}{9:^10d}{10:^10d}{11:^10d}{12:^10d}{13:^10d}{14:^10d}{15:^10d}{16:10d}{17:10d}{18:10d}{19:^15.8f}{20:^15.8f}\n'.format(-999,-9999,-9999,-9999,-9999,-9999,-9999,-9999,j,-9999,-9999,-9999,-9999,-9999,-9999,-9999,-9999,-9999,-9999,ra,dec))
     outf.close()
 
 if __name__ == "__main__":
